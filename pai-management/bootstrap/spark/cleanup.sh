39333505aec1dc5ea2f9787c380fb69e1219961d
FAREAST\t-jizha <t-jizha@microsoft.com>
2018-05-28 14:22:45 +0800

    add seldon

--- pai-management/bootstrap/spark/cleanup.sh
+++ pai-management/bootstrap/spark/cleanup.sh
@@ -1,35 +1,25 @@
 #!/bin/bash
 
-# Copyright (c) Microsoft Corporation
-# All rights reserved.
-#
-# MIT License
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
-# documentation files (the "Software"), to deal in the Software without restriction, including without limitation
-# the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
-# to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED *AS IS*, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
-# BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+echo "delete all resouce in seldon namespaces"
+kubectl delete svc --all -n seldon
+kubectl delete deploy --all -n seldon
+kubectl delete job --all -n seldon
+kubectl delete ds --all -n seldon
+kubectl delete po --all -n seldon
 
-pushd $(dirname "$0") > /dev/null
+while [ -n "$(kubectl get po -n seldon)" ];
+do
+    echo "sleep 5 secs waitting for seldon pod terminate"
+    sleep 5
+done
 
-kubectl delete svc spark-historyserver-web
-kubectl delete svc spark-base
-kubectl delete sts spark-base
+if [ -n "$(kubectl get po -n kube-system | grep ^tiller-deploy)" ]; then
+    echo "helm reset..."
+    helm delete seldon-core-analytics --purge
+    helm delete seldon-core --purge
+    helm delete seldon-core-crd --purge
+    helm reset --force
+    echo "helm reset done"
 
-
-    
-
-    kubectl label nodes spark-master sparkrole-
-    
-
-    
-
-
-popd > /dev/null
+    sleep 5
+fi
